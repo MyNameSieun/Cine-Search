a02c9ea63c5570d94c441265e7369f732ab29254
박시은 <charm516@naver.com>
2024-01-08 12:17:14 +0900

    카드 포지션 에러

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Fetch API를 사용하여 데이터 가져오기
-const movieContainer = document.querySelector("#movieContainer");
+const flip = document.querySelector(".flip");
 
 const url =
   "https://api.themoviedb.org/3/movie/popular?api_key=13b14dad7e58423573b90a27c47ebfbf&language=ko-KR";
@@ -11,8 +11,7 @@ fetch(url)
   .then((data) => {
     allMovies = data.results;
 
-    // 초기에 모든 영화 표시
-    displayMovies(allMovies);
+    displayMovies(allMovies); // 초기에 모든 영화 표시
 
     const searchForm = document.querySelector("form");
 
@@ -33,37 +32,36 @@ fetch(url)
   .catch((error) => {
     console.error("데이터를 가져오는 중 오류 발생:", error);
   });
+//
+
+//
 
 // 영화 카드 표시 함수
 function displayMovies(movies) {
-  movieContainer.innerHTML = ""; // 기존 카드 삭제
+  flip.innerHTML = ""; // 기존 카드 삭제
 
   movies.forEach((movie) => {
-    // card-front 생성 및 표시
-    const card_front = document.createElement("div");
-    card_front.classList.add("card-front");
+    // card_container  생성 및 표시
+    const card_container = document.createElement("div"); // 엘리먼트를 생성
+    card_container.classList.add("card-container"); // "card_container" 클래스를 추가
 
-    card_front.innerHTML = `
-      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title} 포스터" />
-      <div class="card-body">
-        <div class="card-info">
-          <h5 class="card-title">${movie.title}</h5>
-          <p class="star">⭐ ${movie.vote_average}</p>
+    card_container.innerHTML = `
+     <div class="card-front">
+        <img src="https://image.tmdb.org/t/p/w500${
+          movie.poster_path
+        }" class="card-img-top" alt="${movie.title} 포스터" />
+        <div class="card-body">
+          <div class="card-info">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="star">⭐ ${movie.vote_average.toFixed(2)}</p>
+          </div>
         </div>
       </div>
-    `;
-
-    movieContainer.appendChild(card_front);
-
-    // card-back 생성 및 표시
-    const card_back = document.createElement("div");
-    card_back.classList.add("card-back");
-
-    card_back.innerHTML = `
-      <div class="card-body">
+      <div class="card-back">
+        <div class="card-body">
         <div class="card-info">
           <h5 class="card-title">${movie.title}</h5>
-          <p class="star">⭐ ${movie.vote_average}</p>
+          <p class="star">⭐ ${movie.vote_average.toFixed(2)}</p>
         </div>
         <hr class="hr-top" />
         <p class="card-text">
@@ -73,8 +71,9 @@ function displayMovies(movies) {
 
         <a class="btn btn-primary">영화 id</a>
       </div>
-    `;
+    </div>
+      `; // HTML 요소의 내용을 변경
 
-    movieContainer.appendChild(card_back);
+    flip.appendChild(card_container);
   });
 }
