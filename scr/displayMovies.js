d8de3bafa20b6f85e0842e9895d5d33f47663faf
박시은 <charm516@naver.com>
2024-01-09 20:44:31 +0900

    feat: overview 수정

--- scr/displayMovies.js
+++ scr/displayMovies.js
@@ -7,56 +7,36 @@ export function displayMovies(movieList) {
 
     // (4) 생성한 <div> 요소의 내부 HTML을 설정
     // (5) 이때, 각 영화의 정보(제목, 포스터 이미지 경로, 평점, 줄거리 등)를 템플릿 리터럴을 활용하여 동적으로 삽입
-    card_container.innerHTML = `
-       <div class="card-front">
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${
-      movie.title
-    } 포스터" />
-          <div class="card-body">
-            <div class="card-info">
-              <h5 class="card-title">${movie.title}</h5>
-              <p class="star">⭐ ${movie.vote_average.toFixed(2)}</p>
-            </div>
-          </div>
-        </div>
-        <div class="card-back">
-          <div class="card-body">
-          <div class="card-info">
-            <h5 class="card-title">${movie.title}</h5>
-            <p class="star">⭐ ${movie.vote_average.toFixed(2)}</p>
-          </div>
-          <hr class="hr-top" />
-          <p class="card-text">
-            ${movie.overview}
-          </p>
-          <hr class="hr-bottom" />
-  
-          <a class="btn btn-primary">영화 id</a>
-        </div>
-      </div>
-        `;
-
-    // (6) 설정한 내부 HTML을 가진 card_container를 flip 요소의 자식 요소로 추가
-    flip.appendChild(card_container);
-
-    // 버튼 클릭시 영화 id 출력
-    //  querySelectorAll 사용시
-    const movieIdBtnList = card_container.querySelectorAll(".btn");
-    movieIdBtnList.forEach(function (btn) {
-      btn.addEventListener("click", function () {
-        const movieId = movie.id;
-        alert(`영화 id : ${movieId}`);
-      });
-    });
-    // querySelector 사용시
-    // const movieIdBtn = card_container.querySelector(".btn");
-    // movieIdBtn.addEventListener("click", function () {
-    //   const movieId = movie.id;
-    //   alert(`영화 id : ${movieId}`);
-    // });
-
-    // 코드 리팩토링이 필요할듯하다!
     if (movie.overview == "") {
+      card_container.innerHTML = `
+    <div class="card-front">
+       <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${
+        movie.title
+      } 포스터" />
+       <div class="card-body">
+         <div class="card-info">
+           <h5 class="card-title">${movie.title}</h5>
+           <p class="star">⭐ ${movie.vote_average.toFixed(2)}</p>
+         </div>
+       </div>
+     </div>
+     <div class="card-back">
+       <div class="card-body">
+       <div class="card-info">
+         <h5 class="card-title">${movie.title}</h5>
+         <p class="star">⭐ ${movie.vote_average.toFixed(2)}</p>
+       </div>
+       <hr class="hr-top" />
+       <p class="card-text">
+         영화 정보가 없습니다.
+       </p>
+       <hr class="hr-bottom" />
+
+       <a class="btn btn-primary">영화 id</a>
+     </div>
+   </div>
+     `;
+    } else {
       card_container.innerHTML = `
       <div class="card-front">
          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${
@@ -77,13 +57,33 @@ export function displayMovies(movieList) {
          </div>
          <hr class="hr-top" />
          <p class="card-text">
-           정보가 없습니다.
+           ${movie.overview}
          </p>
          <hr class="hr-bottom" />
+ 
          <a class="btn btn-primary">영화 id</a>
        </div>
      </div>
        `;
     }
+
+    // (6) 설정한 내부 HTML을 가진 card_container를 flip 요소의 자식 요소로 추가
+    flip.appendChild(card_container);
+
+    // 버튼 클릭시 영화 id 출력
+    //  querySelectorAll 사용시
+    const movieIdBtnList = card_container.querySelectorAll(".btn");
+    movieIdBtnList.forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        const movieId = movie.id;
+        alert(`영화 id : ${movieId}`);
+      });
+    });
+    // querySelector 사용시
+    // const movieIdBtn = card_container.querySelector(".btn");
+    // movieIdBtn.addEventListener("click", function () {
+    //   const movieId = movie.id;
+    //   alert(`영화 id : ${movieId}`);
+    // });
   });
 }
