71fe82a4d5ea93f2b208efa5b5581411ee0f7559
jisu <ahteostnqls@naver.com>
2024-01-11 10:45:37 +0900

    feat: 작성폼에 입력 시 등록 버튼 활성화 기능(기본은 비활성화)

--- src/review.js
+++ src/review.js
@@ -3,10 +3,18 @@ const $reviewBtnBox = document.querySelector('.review-btn-box');
 const $btnCancel = document.querySelector('.btn-cancel');
 const $btnSend = document.querySelector('.btn-send');
 
+//작성폼 클릭 시 버튼 영역 활성화
 $reviewTextarea.addEventListener('click', () => {
   $reviewBtnBox.style.display = 'flex';
 });
 
+//취소 버튼 클릭시 버튼 영역 비활성화
 $btnCancel.addEventListener('click', () => {
   $reviewBtnBox.style.display = 'none';
+  $reviewTextarea.value = '';
+});
+
+//작성폼에 입력 시 등록 버튼 활성화
+$reviewTextarea.addEventListener('keyup', () => {
+  $btnSend.removeAttribute('disabled');
 });
