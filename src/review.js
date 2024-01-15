94bec155ae73e6b6c6293fa4bddef599d0d9c225
Jiwon <tj1tj2tj3@naver.com>
2024-01-15 16:09:00 +0900

    리뷰 삭제 후 리뷰 없을 시 작성리뷰없음 메시지 출력

--- src/review.js
+++ src/review.js
@@ -139,6 +139,9 @@ function deleteReviewHtml(deleteTargetId) {
       }
     }
   });
+
+  const $noReviewsEl = document.querySelector("#noReview");
+  $noReviewsEl.style.display = "flex";
 }
 
 //추가 버튼 눌렀을 때 리뷰 바로 추가
